981538d7a1b7ee44613d38c50eb5a6ce2cdc9cad
scortino <stefanocortinovis17@gmail.com>
2021-01-07 12:54:15 +0100

    Don't consider beta_0 for plots

--- project/utils.py
+++ project/utils.py
@@ -17,7 +17,7 @@ def trace_plot(mc, path=None, replace=False, title_prefix=""):
     ax1.set_title(f"{title_prefix}Trace Plot for {len(mc)-1} iterations")
     ax1.set_xlabel("Iteration")
     ax1.set_ylabel("Value Drawn")
-    ax1.plot(range(len(mc)), mc)
+    ax1.plot(range(len(mc)-1), mc[1:])
     ax1.legend([f"beta {i}" for i in range(len(mc[0]))], loc="lower right")
     if path is not None:
         if not os.path.exists(path) or replace:
@@ -30,7 +30,7 @@ def dist_plot(mc, warmup=200, path=None, replace=False, title_prefix=""):
     fig, axs = plt.subplots(k, squeeze=True)
     fig.suptitle(f"{title_prefix}Distribution Plot for Beta after {len(mc)-1} iterations")
     for i, ax in enumerate(axs):
-        sns.kdeplot(mc[warmup:, i], ax=ax)
+        sns.kdeplot(mc[warmup+1:, i], ax=ax)
         ax.set_xlabel(f"beta_{i}")
         ax.set_ylabel("Density")
     if path is not None:
