f4426d46776b2a3a98d610313ef252c19899874f
scortino <stefanocortinovis17@gmail.com>
2021-01-07 12:57:22 +0100

    Reorganize simulation scripts; run algos twice with 800 and 20000 as N_ITER

--- gibbs_simulation.py
+++ gibbs_simulation.py
@@ -1,20 +1,35 @@
+from pathlib import Path
+
 from project.gibbs import GibbsProbit
 from project.utils import load_finney47, trace_plot, dist_plot
 
 
+METHOD = 'multinorm'
+BASE_PATH = Path('./images')
 SEED = 42
 
 # Finney (1947), see paper p. 675
 X, Y = load_finney47()
 
-m = GibbsProbit(prior="non-informative", intercept=True)
-mc = m.fit(X, Y, return_chain=True, n_iter=2000, seed=SEED)
-preds = m.predict(X)
-trace_plot(mc, path="./images/trace_gibbs_noinfo_2000.png", replace=False, title_prefix="Gibbs - ")
-dist_plot(mc, path="./images/dist_gibbs_noinfo_2000.png", replace=False, title_prefix="Gibbs - ")
+# Function that, given n_iter and warmup, runs the algorithm on the data for three different priors
+def simul(n_iter, warmup):
+    PRIOR = "noinfo"
+    m = GibbsProbit(prior="non-informative", intercept=True)
+    mc = m.fit(X, Y, n_iter=n_iter, warmup=warmup, seed=SEED)
+    trace_plot(mc, path=BASE_PATH/f"trace_{METHOD}_{PRIOR}_{n_iter}_warmup_{warmup}.png", replace=False, title_prefix=f"{METHOD.capitalize()} - ")
+    dist_plot(mc, warmup=warmup, path=BASE_PATH/f"dist_{METHOD}_{PRIOR}_{n_iter}_warmup_{warmup}.png", replace=False, title_prefix=f"{METHOD.capitalize()} - ")
 
-m = GibbsProbit(prior="multi-norm", intercept=True)
-mc = m.fit(X, Y, return_chain=True, n_iter=2000, seed=SEED)
-preds = m.predict(X)
-trace_plot(mc, path="./images/trace_gibbs_multinorm_2000.png", replace=False, title_prefix="Gibbs - ")
-dist_plot(mc, path="./images/dist_gibbs_multinorm_2000.png", replace=False, title_prefix="Gibbs - ")
+    PRIOR = "multi-norm"
+    m = GibbsProbit(prior="multi-norm", intercept=True)
+    mc = m.fit(X, Y, n_iter=n_iter, warmup=warmup, seed=SEED)
+    trace_plot(mc, path=BASE_PATH/f"trace_{METHOD}_{PRIOR}_{n_iter}_warmup_{warmup}.png", replace=False, title_prefix=f"{METHOD.capitalize()} - ")
+    dist_plot(mc, warmup=warmup, path=BASE_PATH/f"dist_{METHOD}_{PRIOR}_{n_iter}_warmup_{warmup}.png", replace=False, title_prefix=f"{METHOD.capitalize()} - ")
+
+if __name__ == "__main__":
+    # Same number of iterations as paper
+    N_ITER, WARMUP = 800, 0
+    simul(N_ITER, WARMUP)
+
+    # Larger number of iterations
+    N_ITER, WARMUP = 20000, 0
+    simul(N_ITER, WARMUP)
--- metropolis_simulation.py
+++ metropolis_simulation.py
@@ -1,29 +1,43 @@
 import numpy as np
+from pathlib import Path
 from scipy.stats import multivariate_normal
 
 from project.metropolis import MetropolisProbit
 from project.utils import load_finney47, trace_plot, dist_plot
 
 
+METHOD = 'metropolis'
+BASE_PATH = Path('./images')
 SEED = 42
 
 # Finney (1947), see paper p. 675
 X, Y = load_finney47()
 
-m = MetropolisProbit(prior=None, intercept=True) # non-informative improper prior
-mc = m.fit(X, Y, return_chain=True, n_iter=2000, seed=SEED)
-preds = m.predict(X)
-trace_plot(mc, path="./images/trace_metropolis_noinfo_2000.png", replace=False, title_prefix="Metropolis - ")
-dist_plot(mc, path="./images/dist_metropolis_noinfo_2000.png", replace=False, title_prefix="Metropolis - ")
+# Function that, given n_iter and warmup, runs the algorithm on the data for three different priors
+def simul(n_iter, warmup):
+    PRIOR = "noinfo"
+    m = MetropolisProbit(prior=None, intercept=True) # non-informative improper prior
+    mc = m.fit(X, Y, n_iter=n_iter, warmup=warmup, seed=SEED)
+    trace_plot(mc, path=BASE_PATH/f"trace_{METHOD}_{PRIOR}_{n_iter}_warmup_{warmup}.png", replace=False, title_prefix=f"{METHOD.capitalize()} - ")
+    dist_plot(mc, warmup=warmup, path=BASE_PATH/f"dist_{METHOD}_{PRIOR}_{n_iter}_warmup_{warmup}.png", replace=False, title_prefix=f"{METHOD.capitalize()} - ")
 
-m = MetropolisProbit(prior=lambda beta: multivariate_normal.pdf(beta, mean=[0.0, 0.0, 0.0], cov=np.diag([10.0, 10.0, 10.0]))) # independent normal prior with high variance
-mc = m.fit(X, Y, return_chain=True, n_iter=2000, seed=SEED)
-preds = m.predict(X)
-trace_plot(mc, path="./images/trace_metropolis_highvar_2000.png", replace=False, title_prefix="Metropolis - ")
-dist_plot(mc, path="./images/dist_metropolis_highvar_2000.png", replace=False, title_prefix="Metropolis - ")
+    PRIOR = "highvar"
+    m = MetropolisProbit(prior=lambda beta: multivariate_normal.pdf(beta, mean=[0.0, 0.0, 0.0], cov=np.diag([10.0, 10.0, 10.0]))) # independent normal prior with high variance
+    mc = m.fit(X, Y, n_iter=n_iter, warmup=warmup, seed=SEED)
+    trace_plot(mc, path=BASE_PATH/f"trace_{METHOD}_{PRIOR}_{n_iter}_warmup_{warmup}.png", replace=False, title_prefix=f"{METHOD.capitalize()} - ")
+    dist_plot(mc, warmup=warmup, path=BASE_PATH/f"dist_{METHOD}_{PRIOR}_{n_iter}_warmup_{warmup}.png", replace=False, title_prefix=f"{METHOD.capitalize()} - ")
 
-m = MetropolisProbit(prior=lambda beta: multivariate_normal.pdf(beta, mean=[0.0, 0.0, 0.0], cov=np.diag([1.0, 1.0, 1.0]))) # independent normal prior with low variance
-mc = m.fit(X, Y, return_chain=True, n_iter=2000, seed=SEED)
-preds = m.predict(X)
-trace_plot(mc, path="./images/trace_metropolis_lowvar_2000.png", replace=False, title_prefix="Metropolis - ")
-dist_plot(mc, path="./images/dist_metropolis_lowvar_2000.png", replace=False, title_prefix="Metropolis - ")
+    PRIOR = "lowvar"
+    m = MetropolisProbit(prior=lambda beta: multivariate_normal.pdf(beta, mean=[0.0, 0.0, 0.0], cov=np.diag([1.0, 1.0, 1.0]))) # independent normal prior with low variance
+    mc = m.fit(X, Y, n_iter=n_iter, warmup=warmup, seed=SEED)
+    trace_plot(mc, path=BASE_PATH/f"trace_{METHOD}_{PRIOR}_{n_iter}_warmup_{warmup}.png", replace=False, title_prefix=f"{METHOD.capitalize()} - ")
+    dist_plot(mc, warmup=warmup, path=BASE_PATH/f"dist_{METHOD}_{PRIOR}_{n_iter}_warmup_{warmup}.png", replace=False, title_prefix=f"{METHOD.capitalize()} - ")
+
+if __name__ == "__main__":
+    # Same number of iterations as paper
+    N_ITER, WARMUP = 800, 0
+    simul(N_ITER, WARMUP)
+
+    # Larger number of iterations
+    N_ITER, WARMUP = 20000, 0
+    simul(N_ITER, WARMUP)
