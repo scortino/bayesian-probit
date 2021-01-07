90eba6bc9e88a5dc522224644b37df6af439b764
scortino <stefanocortinovis17@gmail.com>
2021-01-07 12:55:52 +0100

    Fix sampling from truncnorm; precompute matrix multiplications when possible

--- project/gibbs.py
+++ project/gibbs.py
@@ -17,16 +17,24 @@ class GibbsProbit(BaseBayesianProbit):
             X = np.hstack([np.ones((X.shape[0], 1)), X])
         beta = np.linalg.inv(X.T @ X) @ X.T @ Y if beta_0 is None else beta_0
         betas = [beta]
+        Z = np.zeros_like(Y)
+        m = Y == 1
+        if self.prior == "non-informative":
+            XprimeX_inv = np.linalg.inv(X.T @ X)
+        elif self.prior == "multi-norm":
+            # TODO: allow user to specify beta_star and b_star (in __init__?)
+            beta_star = np.zeros_like(beta)
+            b_star_inv = np.linalg.inv(np.eye(len(beta)))
+            beta_tilde_base = np.linalg.inv(b_star_inv + X.T @ X)
+            b_tilde = np.linalg.inv(b_star_inv + X.T @ X)
         for _ in range(n_iter):
-            Z = truncnorm.rvs(0, np.inf, X @ beta, 1) * (2 * Y - 1)
+            eta = X @ beta
+            Z[m] = truncnorm.rvs(-eta[m], np.inf, loc=eta[m], scale=1)
+            Z[~m] = truncnorm.rvs(-np.inf, -eta[~m], loc=eta[~m], scale=1)
             if self.prior == "non-informative":
-                beta = multivariate_normal.rvs(np.linalg.inv(X.T @ X) @ X.T @ Z, np.linalg.inv(X.T @ X))
+                beta = multivariate_normal.rvs(XprimeX_inv @ X.T @ Z, XprimeX_inv)
             elif self.prior == "multi-norm":
-                # TODO: allow user to specify beta_star and b_star (in __init__?)
-                beta_star = np.zeros_like(beta)
-                b_star_inv = np.linalg.inv(np.eye(len(beta)))
-                beta_tilde = np.linalg.inv(b_star_inv + X.T @ X) @ (b_star_inv @ beta_star + X.T @ Z)
-                b_tilde = np.linalg.inv(b_star_inv + X.T @ X)
+                beta_tilde = beta_tilde_base @ (b_star_inv @ beta_star + X.T @ Z)
                 beta = multivariate_normal.rvs(beta_tilde, b_tilde)
             betas.append(beta)
         self.beta = sum(betas[warmup:]) / (n_iter - warmup) # bayesian estimator
